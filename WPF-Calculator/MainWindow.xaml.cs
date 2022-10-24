1d4dbc2ada8d659d63c9d4f45ac16e5d9e7bde6a
eminn <eminn@DESKTOP-3IALIBH>
2022-10-24 08:04:26 +0400

    Proje dosyası ekle.

---
+++ WPF-Calculator/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string CurrentValue { get; set; }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentValue += (sender as Button).Content.ToString();
+            current_lbl.Content += CurrentValue;
+        }
+    }
+}
